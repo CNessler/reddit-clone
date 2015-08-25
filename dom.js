8e8ed8e802def258b971e401f966588b9adedf8d
Claire Nessler <clrnessler@gmail.com>
2015-08-25 10:07:08 -0600

    removed unnecessary code

--- dom.js
+++
@@ -1,6 +0,0 @@
-function change() {
-  var changed = document.getElementById('input');
-  changed.value = "BoJack"
-  var insert = document.getElementById('change');
-  insert.innerHTML= changed;
-}
